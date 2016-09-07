3f5ca843a25fc62d09cf8b13895d6fad327ebd20
Ali2ameer <mdameerali24@gmail.com>
2016-09-07 22:00:10 +0530

    Add files via upload

---
+++ pos.c
@@ -0,0 +1,17 @@
+main()
+{
+  int n;
+  scanf("%d",&n);
+  if(n==0)
+  {
+	printf("Given number is zero");
+  }
+  else if(n>=0)
+  {
+	printf("Given number is positive number");
+  }
+  else
+  {
+ 	printf("Given number is nagative number");
+  }
+}
