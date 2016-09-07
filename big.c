434e3a1e683990ff004c202aef69b8a4e1227e7f
Ali2ameer <mdameerali24@gmail.com>
2016-09-07 22:35:29 +0530

    Add files via upload

---
+++ big.c
@@ -0,0 +1,17 @@
+main()
+{
+ int a,b,c;
+ scanf("%d %d %d",&a,$b,&c);
+ if(a>=b && a>c)
+ {
+ 	printf("%d is bigger",a);
+ }
+ else if(b>=a && b>=c)
+ {
+  	printf("%d is bigger",b);
+ }
+ else
+ {
+	printf("%d is bigger",c);
+ }
+}
