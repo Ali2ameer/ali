db810dca6cf90ba4c13b1a505bcad0d53c9b100d
Ali2ameer <mdameerali24@gmail.com>
2016-09-10 22:17:01 +0530

    Add files via upload

---
+++ ovel.c
@@ -0,0 +1,14 @@
+main()
+{
+ char c;
+ printf("Enter a charecter :\n");
+ scanf("%c",&c);
+ if(c=='a'c=='A'|| c=='e' c=='E' || c=='i'c=='I' c=='o' c=='O'|| c=='u' c=='U')
+ {
+	printf("It is Ovel");
+ }
+ else
+ {
+ 	printf("It is Consonant");
+ }
+}
